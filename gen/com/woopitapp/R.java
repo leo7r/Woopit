ebc656f008c038efd3c7804932269e0daabcfa1f
leo7r <ramosleo12@gmail.com>
2014-05-06 21:53:26 -0430

    Creada notificacion para hacer el video, se desactiva poniendo bool notif=false en MainActivity

--- gen/com/woopitapp/R.java
+++ gen/com/woopitapp/R.java
@@ -24,22 +24,26 @@ public final class R {
         public static final int box_texture=0x7f020000;
         public static final int box_texture2=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int images=0x7f020003;
+        public static final int notif_icon=0x7f020003;
+        public static final int profile2=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f08000a;
         public static final int botonSuma=0x7f080007;
+        public static final int image=0x7f080008;
         public static final int latitud=0x7f080001;
         public static final int longitud=0x7f080003;
         public static final int rdbOne=0x7f080005;
         public static final int rdbTwo=0x7f080006;
         public static final int rdgGrupo=0x7f080004;
         public static final int texto=0x7f080000;
+        public static final int texto1=0x7f080009;
         public static final int texto2=0x7f080002;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int notification=0x7f030002;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
--- src/com/woopitapp/MainActivity.java
+++ src/com/woopitapp/MainActivity.java
@@ -8,7 +8,11 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -23,10 +27,11 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.opengl.GLSurfaceView;
-import android.opengl.GLU;
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
 import android.opengl.GLUtils;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
@@ -37,6 +42,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -61,6 +67,8 @@ public class MainActivity extends Activity {
 	int indice = 0;
 	GLClearRenderer render;
 	
+	boolean notif = true;
+	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +76,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.box_texture2);
         Button bSuma = (Button) findViewById(R.id.botonSuma);
+        
         bSuma.setOnClickListener(new OnClickListener(){
         	@Override
 			public void onClick(View arg0) {
@@ -75,7 +84,18 @@ public class MainActivity extends Activity {
 				crearCamara();
 			}
         });
-     
+        
+        if ( notif ){
+        	new Handler().postDelayed(new Runnable(){
+
+				@Override
+				public void run() {
+		        	setNotification();
+				}
+			}, 10000);
+        	
+        }
+        
     }
 	
 	/* Imagen en 3D */
@@ -659,6 +679,23 @@ public class MainActivity extends Activity {
     public void recopilar(int num) {
     	TextView editText = (TextView) findViewById(R.id.texto);
     	editText.setText(editText.getText() + "" + num);
+    }
+    
+    /* Video notificacion */
+    public void setNotification(){
+    	
+    	Notification notification = new Notification(R.drawable.notif_icon, null, System.currentTimeMillis());
+		RemoteViews notificationView = new RemoteViews(getPackageName(), R.layout.notification);
+	    
+    	
+	    Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+	    PendingIntent pendingNotificationIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_ONE_SHOT);
+
+	    notification.contentView = notificationView;
+	    notification.contentIntent = pendingNotificationIntent;
+	    
+	    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    	mNotificationManager.notify(1, notification);
     }
     
 }
