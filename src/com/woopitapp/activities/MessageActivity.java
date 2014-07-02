924cbceaa05692c5229744b90f3fbdddc9c5b967
Alejandro <alejandroballestae@gmail.com>
2014-07-01 22:23:44 -0500

    otra vez

--- src/com/woopitapp/activities/MessageActivity.java
+++ src/com/woopitapp/activities/MessageActivity.java
@@ -50,9 +50,11 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorListener;
 import android.hardware.SensorManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.audiofx.BassBoost.Settings;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLSurfaceView.Renderer;
@@ -318,7 +320,9 @@ public class MessageActivity extends Activity {
           
           LocationManager locMan;
           locMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-          locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER,100, 1, gpsListener);
+          LocationChangeListener prueba = new LocationChangeListener(this.getContext());
+          locMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 4000, 0, prueba);
+          locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 4000, 0, prueba);
           previewHolder = this.getHolder();
           previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
           previewHolder.addCallback(surfaceHolderListener);
@@ -524,7 +528,7 @@ public class MessageActivity extends Activity {
         double brng = Math.toDegrees(Math.atan2(y, x));
     	return brng;
     }
-    
+ 
     protected float[] lowPass(float[] input, float[] output){
         
     	if (output == null)
@@ -617,5 +621,196 @@ public class MessageActivity extends Activity {
 		}
     	
     }
-    
+    public class LocationChangeListener  implements LocationListener{
+
+
+    	private LocationManager mLocationManager = null;
+    	private static final int TWO_MINUTES = 1000 * 60 * 2;
+    	private Location currentBestLocation = null;
+
+    	public LocationChangeListener(Context context) {
+    	    super();
+    	}
+
+    	public void impl(int refreshPeriod, int accuracyInMeters){
+
+
+
+    	    // Get the location manager
+    	    mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+    	    currentBestLocation = getLastBestLocation();
+
+    	    boolean enabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+    	    // Check if enabled and if not send user to the GSP settings
+    	    // Better solution would be to display a dialog and suggesting to 
+    	    // go to the settings
+    	    if (!enabled) {
+    	    
+    	    } 
+
+
+    	    Criteria criteria = new Criteria();
+    	    //criteria.setAccuracy(Criteria.ACCURACY_FINE);
+    	    //criteria.setCostAllowed(false);
+
+    	    String provider = mLocationManager.getBestProvider(criteria, false);
+    	    Location location = mLocationManager.getLastKnownLocation(provider);
+
+    	    LocationListener listener = this;       
+    	    mLocationManager.requestLocationUpdates(provider, refreshPeriod, accuracyInMeters, listener);
+    	    //mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+    	    //mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
+
+    	    this.onLocationChanged(location);
+
+    	    // Initialize the location fields
+    	    if (location == null) {
+    	        //Location not available
+
+    	        mLocationManager.removeUpdates(this);
+    	    }
+
+    	}
+
+    	/**
+    	 * This method returns the last know location, between the GPS and the Network one.
+    	 * For this method newer is best :)
+    	 * 
+    	 * @return the last know best location
+    	 */
+    	private Location getLastBestLocation() {
+    	    Location locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    	    Location locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+    	    long GPSLocationTime = 0;
+    	    if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
+
+    	    long NetLocationTime = 0;
+
+    	    if (null != locationNet) {
+    	        NetLocationTime = locationNet.getTime();
+    	    }
+
+    	    if ( 0 < GPSLocationTime - NetLocationTime ) {
+    	        return locationGPS;
+    	    }
+    	    else{
+    	        return locationNet;
+    	    }
+
+    	}
+
+    	/**
+    	 * This method modify the last know good location according to the arguments.
+    	 * 
+    	 * @param location      the possible new location
+    	 */
+    	void makeUseOfNewLocation(Location location) {
+    	    if ( isBetterLocation(location, currentBestLocation) ) {
+    	        currentBestLocation = location;
+    	    }
+    	}
+    	private double convertir(double i){
+    		return i*(-10.0)/0.02;
+    	}
+    	public void onLocationChanged(Location location) {
+
+    	    makeUseOfNewLocation(location);
+
+    	    if(currentBestLocation == null){
+    	        currentBestLocation = location;
+    	    }
+	
+ 			
+ 			angulo = getBearing(Double.parseDouble(latitud),Double.parseDouble(longitud),location.getLatitude(), location.getLongitude());
+ 			double distancia = getDistance(Double.parseDouble(latitud),Double.parseDouble(longitud),location.getLatitude(), location.getLongitude());
+ 			//distancia = convertir(distancia);
+ 			direccion.setText(distancia + "");
+ 			if(render != null && Double.parseDouble(latitud) < 500.0){
+ 				render.transladarMundo(((float) (distancia+0.190)*-100.0f));
+ 			}
+       
+    	}
+
+    	public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    	}
+
+    	public void onProviderEnabled(String provider) {
+
+
+    	}
+
+    	public void onProviderDisabled(String provider) {
+
+
+    	}   
+
+    	/** Determines whether one Location reading is better than the current Location fix
+    	 * @param location  The new Location that you want to evaluate
+    	 * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+    	 */
+    	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+    	    if (currentBestLocation == null) {
+    	        // A new location is always better than no location
+    	        return true;
+    	    }
+
+    	    // Check whether the new location fix is newer or older
+    	    long timeDelta = location.getTime() - currentBestLocation.getTime();
+    	    boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+    	    boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+    	    boolean isNewer = timeDelta > 0;
+
+    	    // If it's been more than two minutes since the current location, use the new location
+    	    // because the user has likely moved
+    	    if (isSignificantlyNewer) {
+    	        return true;
+    	        // If the new location is more than two minutes older, it must be worse
+    	    } else if (isSignificantlyOlder) {
+    	        return false;
+    	    }
+
+    	    // Check whether the new location fix is more or less accurate
+    	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+    	    boolean isLessAccurate = accuracyDelta > 0;
+    	    boolean isMoreAccurate = accuracyDelta < 0;
+    	    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+    	    // Check if the old and new location are from the same provider
+    	    boolean isFromSameProvider = isSameProvider(location.getProvider(),
+    	            currentBestLocation.getProvider());
+
+    	    // Determine location quality using a combination of timeliness and accuracy
+    	    if (isMoreAccurate) {
+    	        return true;
+    	    } else if (isNewer && !isLessAccurate) {
+    	        return true;
+    	    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+    	        return true;
+    	    }
+    	    return false;
+    	}
+
+    	/** Checks whether two providers are the same */
+    	private boolean isSameProvider(String provider1, String provider2) {
+    	    if (provider1 == null) {
+    	        return provider2 == null;
+    	    }
+    	    return provider1.equals(provider2);
+    	}
+
+
+
+    	public void destroyManager() {
+
+    	    if(mLocationManager != null){
+    	        mLocationManager.removeUpdates(this);
+
+    	        mLocationManager = null;
+    	    }       
+    	}
+    	}
 }
